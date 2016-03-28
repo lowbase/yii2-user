ea56236324d0de30a2b3aa25d03212bbb23d4e6d
lowBase <lowbase@yandex.ru>
2016-03-29 00:18:36 +0300

    Update ResetPassword.php

--- models/ResetPassword.php
+++ models/ResetPassword.php
@@ -20,15 +20,14 @@ class ResetPassword extends Model
     public $password;
 
     /**
-     * @var \common\models\User
+     * @var \lowbase\user\models\user
      */
     private $_user;
 
     /**
-     * Creates a form model given a token.
-     * @param  string                          $token
-     * @param  array                           $config name-value pairs that will be used to initialize the object properties
-     * @throws \yii\base\InvalidParamException if token is empty or not valid
+     * @param array $token
+     * @param $password
+     * @param array $config
      */
     public function __construct($token, $password, $config = [])
     {
@@ -47,8 +46,7 @@ class ResetPassword extends Model
     }
 
     /**
-     * Resets password.
-     * @return boolean if password was reset.
+     * @return bool|int|mixed
      */
     public function resetPassword()
     {
