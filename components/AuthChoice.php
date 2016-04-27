c2107a1326ddd2f27af5c97a470fd4151be85ba8
lowBase <lowbase@yandex.ru>
2016-04-27 16:52:06 +0300

    Update AuthChoice.php

--- components/AuthChoice.php
+++ components/AuthChoice.php
@@ -14,127 +14,26 @@ use Yii;
 use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\authclient\ClientInterface;
-use yii\authclient\widgets\AuthChoiceAsset;
 
 /**
- * Используется для изображения представления
+ * Авторизация через соц. сети.
+ * Используется стандартный код класса.
+ * Изменения касаются лишь тегов оформления
+ * отображения
+ *
  * Class AuthChoice
  * @package app\modules\users\components
  */
 class AuthChoice extends \yii\authclient\widgets\AuthChoice
 {
-    /**
-     * @var string name of the auth client collection application component.
-     * This component will be used to fetch services value if it is not set.
-     */
-    public $clientCollection = 'authClientCollection';
-
-    public $clientCssClass = 'col-xs-1';
-    /**
-     * @var string name of the GET param , which should be used to passed auth client id to URL
-     * defined by [[baseAuthUrl]].
-     */
-    public $clientIdGetParamName = 'authclient';
-    /**
-     * @var array the HTML attributes that should be rendered in the div HTML tag representing the container element.
-     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
-     */
-    public $options = [
-        'class' => ''
-    ];
-    /**
-     * @var boolean indicates if popup window should be used instead of direct links.
-     */
-    public $popupMode = true;
-    /**
-     * @var boolean indicates if widget content, should be rendered automatically.
-     * Note: this value automatically set to 'false' at the first call of [[createClientUrl()]]
-     */
-    public $autoRender = true;
-    /**
-     * @var array configuration for the external clients base authentication URL.
-     */
-    private $_baseAuthUrl;
-    /**
-     * @var ClientInterface[] auth providers list.
-     */
-    private $_clients;
-
+    public $clientCssClass = 'col-xs-1'; // 12 иконок в ряд
 
     /**
-     * @param ClientInterface[] $clients auth providers
-     */
-    public function setClients(array $clients)
-    {
-        $this->_clients = $clients;
-    }
-
-    /**
-     * @return ClientInterface[] auth providers
-     */
-    public function getClients()
-    {
-        if ($this->_clients === null) {
-            $this->_clients = $this->defaultClients();
-        }
-
-        return $this->_clients;
-    }
-
-    /**
-     * @param array $baseAuthUrl base auth URL configuration.
-     */
-    public function setBaseAuthUrl(array $baseAuthUrl)
-    {
-        $this->_baseAuthUrl = $baseAuthUrl;
-    }
-
-    /**
-     * @return array base auth URL configuration.
-     */
-    public function getBaseAuthUrl()
-    {
-        if (!is_array($this->_baseAuthUrl)) {
-            $this->_baseAuthUrl = $this->defaultBaseAuthUrl();
-        }
-
-        return $this->_baseAuthUrl;
-    }
-
-    /**
-     * Returns default auth clients list.
-     * @return ClientInterface[] auth clients list.
-     */
-    protected function defaultClients()
-    {
-        /* @var $collection \yii\authclient\Collection */
-        $collection = Yii::$app->get($this->clientCollection);
-
-        return $collection->getClients();
-    }
-
-    /**
-     * Composes default base auth URL configuration.
-     * @return array base auth URL configuration.
-     */
-    protected function defaultBaseAuthUrl()
-    {
-        $baseAuthUrl = [
-            Yii::$app->controller->getRoute()
-        ];
-        $params = $_GET;
-        unset($params[$this->clientIdGetParamName]);
-        $baseAuthUrl = array_merge($baseAuthUrl, $params);
-
-        return $baseAuthUrl;
-    }
-
-    /**
-     * Outputs client auth link.
-     * @param ClientInterface $client external auth client instance.
-     * @param string $text link text, if not set - default value will be generated.
-     * @param array $htmlOptions link HTML options.
-     * @throws InvalidConfigException on wrong configuration.
+     * Добавляем обрамление div
+     * @param ClientInterface $client
+     * @param null $text
+     * @param array $htmlOptions
+     * @throws InvalidConfigException
      */
     public function clientLink($client, $text = null, array $htmlOptions = [])
     {
@@ -175,21 +74,8 @@ class AuthChoice extends \yii\authclient\widgets\AuthChoice
     }
 
     /**
-     * Composes client auth URL.
-     * @param ClientInterface $provider external auth client instance.
-     * @return string auth URL.
-     */
-    public function createClientUrl($provider)
-    {
-        $this->autoRender = false;
-        $url = $this->getBaseAuthUrl();
-        $url[$this->clientIdGetParamName] = $provider->getId();
-
-        return Url::to($url);
-    }
-
-    /**
-     * Renders the main content, which includes all external services links.
+     * Меняем ul на div
+     * @throws InvalidConfigException
      */
     protected function renderMainContent()
     {
@@ -200,30 +86,4 @@ class AuthChoice extends \yii\authclient\widgets\AuthChoice
         echo Html::endTag('div');
     }
 
-    /**
-     * Initializes the widget.
-     */
-    public function init()
-    {
-        $view = Yii::$app->getView();
-        if ($this->popupMode) {
-            AuthChoiceAsset::register($view);
-            $view->registerJs("\$('#" . $this->getId() . "').authchoice();");
-        } else {
-            AuthChoiceStyleAsset::register($view);
-        }
-        $this->options['id'] = $this->getId();
-        echo Html::beginTag('div', $this->options);
-    }
-
-    /**
-     * Runs the widget.
-     */
-    public function run()
-    {
-        if ($this->autoRender) {
-            $this->renderMainContent();
-        }
-        echo Html::endTag('div');
-    }
 }
