d8f88d83bb7f11c41c04ecdfecfab4837d97ef38
lowBase <lowbase@yandex.ru>
2016-04-27 03:33:30 +0300

    Update ProfileForm.php

--- models/forms/ProfileForm.php
+++ models/forms/ProfileForm.php
@@ -19,39 +19,39 @@ use Yii;
  */
 class ProfileForm extends User
 {
-    public $password;
-    public $captcha;
+    public $password;   // Новый пароль
 
     /**
-     * @return array the validation rules.
+     * Правила валдиации
+     * @return array
      */
     public function rules()
     {
         return [
-            [['first_name'], 'required'],
-            [['password'], 'passwordValidate', 'skipOnEmpty' => false],
-            [['email'], 'emailValidate', 'skipOnEmpty' => false],
+            [['first_name'], 'required'],   // Обязательно для заполнения
+            [['password'], 'passwordValidate', 'skipOnEmpty' => false], // Валидация пароля методом модели
+            [['email'], 'emailValidate', 'skipOnEmpty' => false],   // Валидация электронной почты методом модели
             ['email', 'unique', 'targetClass' => self::className(),
-                'message' => Yii::t('user', 'Данный Email уже зарегистрирован.')],
-            ['email', 'email'],
-            [['password'], 'string', 'min' => 4],
-            [['sex', 'country_id', 'city_id', 'status'], 'integer'],
-            [['birthday', 'login_at'], 'safe'],
-            [['first_name', 'last_name', 'email', 'phone'], 'string', 'max' => 100],
-            [['auth_key'], 'string', 'max' => 32],
-            [['ip'], 'string', 'max' => 20],
-            [['password_hash', 'password_reset_token', 'email_confirm_token', 'image', 'address'], 'string', 'max' => 255],
-            ['status', 'in', 'range' => array_keys(self::getStatusArray())],
-            ['status', 'default', 'value' => self::STATUS_WAIT],
-            ['sex', 'in', 'range' => array_keys(self::getSexArray())],
-            [['first_name', 'last_name', 'email', 'phone', 'address'], 'filter', 'filter' => 'trim'],
+                'message' => Yii::t('user', 'Данный Email уже зарегистрирован.')],  // Электронная почта должна быть уникальна
+            ['email', 'email'], // Электронная почта
+            [['password'], 'string', 'min' => 4],   // Пароль минимум 4 символа
+            [['sex', 'country_id', 'city_id', 'status'], 'integer'],    // Целочисленные значения
+            [['birthday', 'login_at'], 'safe'], // Безопасные аттрибуты
+            [['first_name', 'last_name', 'email', 'phone'], 'string', 'max' => 100], // Строковые значения (максимум 100 символов)
+            [['auth_key'], 'string', 'max' => 32],  // Строковое значение (максимум 32 символа)
+            [['ip'], 'string', 'max' => 20],    // Строковое значение (максимум 20 симоволов)
+            [['password_hash', 'password_reset_token', 'email_confirm_token', 'image', 'address'], 'string', 'max' => 255], // Строка (максимум 255 символов)
+            ['status', 'in', 'range' => array_keys(self::getStatusArray())], // Статус должен быть из списка статусов
+            ['sex', 'in', 'range' => array_keys(self::getSexArray())], // Пол должен быть из гендерного списка
+            [['first_name', 'last_name', 'email', 'phone', 'address'], 'filter', 'filter' => 'trim'],   // Обрезание строк по краям
             [['last_name', 'password_reset_token', 'email_confirm_token',
                 'image', 'sex', 'phone', 'country_id', 'city_id', 'address',
-                'auth_key', 'password_hash', 'email', 'ip', 'login_at'], 'default', 'value' => null],
+                'auth_key', 'password_hash', 'email', 'ip', 'login_at'], 'default', 'value' => null],   // По умолчанию = null
         ];
     }
 
     /**
+     * Дополнительные поля формы
      * @return array
      */
     public function attributeLabels()
@@ -61,18 +61,23 @@ class ProfileForm extends User
         return $labels;
     }
 
-     /**
+    /**
+     * Генерация пароля и ключа авторизации,
+     * преобразование дня рождения в необходимый
+     * формат перед сохранением
+     * 
      * @param bool $insert
-     * @param $changedAttributes
      * @return bool
      */
     public function beforeSave($insert)
     {
         if (parent::beforeSave($insert)) {
+            // Если указан новый пароль
             if ($this->password) {
                 $this->setPassword($this->password);
                 $this->generateAuthKey();
             }
+            // Преобразование дня рождения
             if ($this->birthday) {
                 $date = new \DateTime($this->birthday);
                 $this->birthday = $date->format('Y-m-d');
@@ -83,8 +88,10 @@ class ProfileForm extends User
     }
 
     /**
-     * @param $attribute
-     * @param $params
+     * Валидация пароля
+     * 
+     * Указывается обязательно при отсутствии
+     * ключей авторизации соц. сетей
      */
     public function passwordValidate()
     {
@@ -94,8 +101,10 @@ class ProfileForm extends User
     }
 
     /**
-     * @param $attribute
-     * @param $params
+     * Валидация электронной почты
+     * 
+     * Указывается обязательно при отсутствии
+     * ключей авторизации соц. сетей
      */
     public function emailValidate()
     {
