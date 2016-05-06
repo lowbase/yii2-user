2e3ba03605d89a63076274d659d95be1e0d936bc
lowBase <lowbase@yandex.ru>
2016-05-06 19:04:49 +0300

    Update index.php

--- views/auth-item/index.php
+++ views/auth-item/index.php
@@ -23,11 +23,6 @@ $assets = UserAsset::register($this);
 ?>
 <div class="auth-item-index">
 
-    <div class="row">
-        <div class="col-lg-12">
-            <?= $this->render(Yii::$app->controller->module->getCustomView('alert', '../default/alert'));?>
-        </div>
-    </div>
     <?php
     $gridColumns = [
         [
