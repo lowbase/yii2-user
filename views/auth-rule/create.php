ab3acb55cdb2f26053ea67e6c7f60c6612a9ff47
lowBase <lowbase@yandex.ru>
2016-05-06 19:05:49 +0300

    Update create.php

--- views/auth-rule/create.php
+++ views/auth-rule/create.php
@@ -21,7 +21,6 @@ $assets = UserAsset::register($this);
 
     <div class="row">
         <div class="col-lg-12">
-            <?=$this->render(Yii::$app->controller->module->getCustomView('alert', '../default/alert'));?>
             <h1><?= Html::encode($this->title) ?></h1>
         </div>
     </div>
